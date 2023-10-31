4121427adfacf6125b5dac690719a4f053847247
Jason Dubon <jasonsdubon@gmail.com>
2023-10-31 08:20:59 -0400

    Initial Commit

---
+++ StreamTV/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StreamTV
+//
+//  Created by Jason Dubon on 10/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StreamTV/StreamTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamTVApp.swift
+//  StreamTV
+//
+//  Created by Jason Dubon on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct StreamTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
